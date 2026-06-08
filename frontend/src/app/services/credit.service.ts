f4a8f237f9fda7983262f827cd5bbccd4c1b9a9d
hugoCode3125 <leonthiumouedraogo@gmail.com>
2026-06-08 12:46:53 +0100

    final

--- frontend/src/app/services/credit.service.ts
+++ frontend/src/app/services/credit.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CreditService {
@@ -8,19 +9,36 @@ export class CreditService {
 
   constructor(private http: HttpClient) {}
 
-  getCredits(clientId: number) {
-    return this.http.get<any[]>(`${this.api}/credits/client/${clientId}`);
+  // Génération dynamique des headers avec le token JWT
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token'); // Assure-toi que la clé est bien 'token'
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
   }
 
-  addPersonnel(data: any) {
-    return this.http.post(`${this.api}/credits/personnel`, data);
+  // Utilisation d'un type d'ID robuste et passage des headers requis
+  getCredits(clientId: number | string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.api}/credits/client/${clientId}`, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-  addImmobilier(data: any) {
-    return this.http.post(`${this.api}/credits/immobilier`, data);
+  addPersonnel(data: any): Observable<any> {
+    return this.http.post(`${this.api}/credits/personnel`, data, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-  addProfessionnel(data: any) {
-    return this.http.post(`${this.api}/credits/professionnel`, data);
+  addImmobilier(data: any): Observable<any> {
+    return this.http.post(`${this.api}/credits/immobilier`, data, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+  addProfessionnel(data: any): Observable<any> {
+    return this.http.post(`${this.api}/credits/professionnel`, data, {
+      headers: this.getAuthHeaders()
+    });
   }
 }
